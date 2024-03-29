540c55e7c579fc01fad958dde4c0cfb69a4e1f55
Sv8 <sergiovargasumacacho@gmail.com>
2024-03-29 14:58:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,288 @@
+let categorias = [
+  {
+    NombreCategoria: "Mejor Videojuego",
+    Portada: "./assets/PortadasCat/CatMejorGame.jpeg",
+    Nominados: [
+      {
+        id: 1,
+        nombre: "Rocket League",
+        imagen: "./assets/Juegos/Rocket League.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/rNoHfFAdO4A?si=ZB3YTiLSGqbT66nI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      },
+      {
+        id: 2,
+        nombre: "Amnesia",
+        imagen: "./assets/Juegos/Amnesia.jpg",
+        preview: ''
+      },
+      {
+        id: 3,
+        nombre: "GTA V",
+        imagen: "./assets/Juegos/GTA V.jpg",
+        preview: ''
+      },
+      {
+        id: 4,
+        nombre: "Brawhalla",
+        imagen: "./assets/Juegos/BRAWHALLA.jpg",
+        preview: ''
+      },
+      {
+        id: 5,
+        nombre: "Black Ops 3",
+        imagen: "./assets/Juegos/COD BO3.jpg",
+        preview: ''
+      },
+      {
+        id: 6,
+        nombre: "Omori",
+        imagen: "./assets/Juegos/Omori.jpg",
+        preview: ''
+      },
+    ],
+    Ganador: 0
+  },
+
+  {
+    NombreCategoria: "Mejor Serie ",
+    Portada: "./assets/PortadasCat/",
+    Nominados: [
+      {
+        id: 1,
+        nombre: "South park",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Ov_b94XAYPU?si=NUq9KynU3XPbTvqo&amp;start=157" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 2,
+        nombre: "Las dos series xd",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/07P4LK-GJOM?si=HJaz81QpCTsIffma&amp;start=157" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 3,
+        nombre: "Casa de papel",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/KQ7QVchnNb0?si=8G4L0QAVMHcrXZz2&amp;start=157" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 4,
+        nombre: "Breaking Bad",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/qhxgxbLYstU?si=A7huXAFBkKRL5u4v&amp;start=136" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 5,
+        nombre: "Invencible",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/vlPqWWPiPTA?si=72BU5IeMwQYjODsg&amp;start=136" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 6,
+        nombre: "Rick and Morty",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Ov_b94XAYPU?si=7XO4sLBaELyAqA0u&amp;start=136" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      }
+    ],
+    Ganador: 0
+  },
+
+  {
+    NombreCategoria: "Mejor Pelicula",
+    Portada: "./assets/PortadasCat/",
+    Nominados: [
+      {
+        id: 1,
+        nombre: "American Psycho",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/6SFs655p3Eo?si=aWvaBh6GM_VsIK__&amp;start=136" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 2,
+        nombre: "El club de la pelea",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/i-Yq2T_EIpY?si=lLTrjwnv_bqyW-aB&amp;start=136" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 3,
+        nombre: "Chiquito pero peligroso",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/I9oXQcz7pQI?si=rBO6MraQRiNsCMW6&amp;start=136" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 4,
+        nombre: "Black Phone",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/0weOo_8NhAA?si=irKdXTvH30MyDitc&amp;start=136" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 5,
+        nombre: "Kizumonogatari 1-2-3",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/fUG8EfzNlFU?si=VqeMy0sJbXW_f4zS&amp;start=136" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+    ],
+    Ganador: 0
+  },
+
+  {
+    NombreCategoria: "Mejor Anime",
+    Portada: "./assets/PortadasCat/",
+    Nominados: [
+      {
+        id: 1,
+        nombre: "Ore wo suki",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/DXfTuDVUYQQ?si=9KYA4LKZq_0Hm6qT&amp;start=98" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 2,
+        nombre: "Oregairu",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/m6V_urNf8zA?si=nfm178pKf3IS_2EQ&amp;start=438" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 3,
+        nombre: "Re Zero",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/9nMFDnmyCrE?si=6AGkXNEgLht88Bb5&amp;start=91" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 4,
+        nombre: "Konosuba",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/rd9877R55Tg?si=ApZTWo9OO59dUenI&amp;start=32" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 5,
+        nombre: "Grand Blue ",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/DzVQP7a518E?si=CjpbGTwCkY71Rdtf&amp;start=32" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      },
+      {
+        id: 6,
+        nombre: "La conejita",
+        imagen: "./assets/portada.jpg",
+        preview: '<iframe width="560" height="315" src="https://www.youtube.com/embed/a3wmqGfNrTM?si=kfbVZSKOXlThREbC&amp;start=32" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+      }
+    ],
+    Ganador: 0
+  },
+]
+
+
+let actual = 1;
+
+class Categoria {
+  constructor(nombre, imagenPortada, nominados, ganador) {
+    let article = document.createElement("article");
+    article.classList.add("categoria");
+    this.diapoPortada = new DiapositivaPortada(nombre, imagenPortada);
+    this.diapoNominados = new DiapositivaNominados(nominados, ganador);
+
+    article.append(this.diapoPortada.HTMLElement);
+    article.append(this.diapoNominados.HTMLElement);
+
+    this.HTMLElement = article;
+  }
+  enfocarCategoria() {
+    scroll({
+      left: this.HTMLElement.offsetLeft,
+      top: this.HTMLElement.offsetTop,
+      behavior: "smooth",
+    });
+  }
+  enfocarPortada() {
+    scroll({
+      left: this.diapoPortada.HTMLElement.offsetLeft,
+      top: this.diapoPortada.HTMLElement.offsetTop,
+      behavior: "smooth",
+    });
+  }
+  enfocarNominados() {
+    scroll({
+      left: this.diapoNominados.HTMLElement.offsetLeft,
+      top: this.diapoNominados.HTMLElement.offsetTop,
+      behavior: "smooth",
+    });
+  }
+}
+class DiapositivaNominados {
+  constructor(nominados, ganador) {
+    let diapositiva = document.createElement("section");
+    diapositiva.classList.add("contenedor-nominados");
+    if (nominados.length == 4) {
+      diapositiva.style.gap = '2em 15em'
+    }
+    else {
+      diapositiva.style.gap = '2em'
+    }
+    nominados.forEach((i) => {
+      let nominado = new TarjetaNominado(i.nombre, i.imagen, i.preview);
+      diapositiva.append(nominado.HTMLElement);
+    });
+    this.HTMLElement = diapositiva;
+  }
+}
+class TarjetaNominado {
+  constructor(nombre, imagen, animacion) {
+    let div = document.createElement("div");
+    div.classList.add("nominado");
+
+    let span = document.createElement("span");
+    span.textContent = nombre;
+
+    let img = document.createElement("img");
+    img.src = imagen;
+
+    console.log(animacion)
+    let preview = document.createElement("div")
+    preview.classList.add('preview')
+    preview.innerHTML = animacion
+
+
+    div.append(img);
+    div.append(span);
+    div.append(preview);
+    this.HTMLElement = div;
+  }
+}
+class DiapositivaPortada {
+  constructor(nombre, imagen) {
+    let diapositiva = document.createElement("section");
+    diapositiva.classList.add("portada");
+
+    let titulo = document.createElement("h2");
+    titulo.textContent = nombre;
+
+    let img = document.createElement("img");
+    img.src = imagen;
+
+    diapositiva.appendChild(titulo);
+    diapositiva.appendChild(img);
+
+    this.HTMLElement = diapositiva;
+  }
+}
+
+let cate = new Categoria(
+  categorias[actual].NombreCategoria,
+  categorias[actual].Portada,
+  categorias[actual].Nominados,
+  categorias[actual].Ganador
+);
+document.body.append(cate.HTMLElement);
+
+addEventListener("keyup", (e) => {
+  if (e.key == "a") {
+    cate.enfocarPortada();
+  } else if (e.key == "d") {
+    cate.enfocarNominados();
+  }
+});
+document.querySelectorAll('.nominado').forEach(e => {
+  e.addEventListener('click', () => {
+    e.classList.toggle('visible')
+  })
+})
+
